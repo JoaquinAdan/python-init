b96af70c510eea437dd2360df3df225d9bea496b
JoaquinAdan <rodriguezjdev@gmail.com>
2025-01-31 17:37:04 +0100

    Add new Python scripts demonstrating conditional statements and membership checks

---
+++ clase11_3.py
@@ -0,0 +1,10 @@
+is_member = True
+age = 20
+
+if is_member:
+    if age >= 15:
+        print("Puedes entrar porque sos miembre y sos mayor o igual a 15 años")
+    else:
+        print("No puedes entrar porque sos menor de 15 años, aunque seas miembro")
+else:
+    print("No puedes entrar porque no sos miembro")
