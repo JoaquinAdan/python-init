bf81222d4f33ed3d9d535c38a22261111690c482
JoaquinAdan <rodriguezjdev@gmail.com>
2025-02-09 00:44:01 +0100

    Add asynchronous order processing with inventory checks and payment simulation

--- concurrencia-paralelismo-asincronismo/asincronia.py
+++ concurrencia-paralelismo-asincronismo/asincronia.py
@@ -17,3 +17,17 @@ async def main():
 
 
 asyncio.run(main())
+
+# ________________________________________________________________________
+# |                       |                       |                       |
+# |       CONCEPTO        |     CONCURRENCIA      |      ASINCRONÍA       |
+# |_______________________|_______________________|_______________________|
+# |                       | Ejecución intercalada |  Tareas se suspenden  |
+# |       Definición      |      de tareas        |      y reanudan       |
+# |_______________________|_______________________|_______________________|
+# |                       | Tareas de CPU e I/O   |      Operaciones      |
+# |      Usos ideales     |       Intensivo       |  dependientes de I/O  |
+# |_______________________|_______________________|_______________________|
+# |                       |    multiprocessing    |                       |
+# |     Implementación    |       threading       |        asyncio        |
+# |_______________________|_______________________|_______________________|
